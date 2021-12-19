888fda76e0c008921e44882e8efb5676be62f0c9
Untitled <mps86@naver.com>
2021-12-19 10:26:46 +0000

    백준 1543번 문서검색 단순 탐색 알고리즘

---
+++ 1543.py
@@ -0,0 +1,11 @@
+document = input()
+word = input()
+index = 0
+result = 0
+while len(document) - index >= len(word):
+    if document[index:index + len(word)] == word: # 문서에서 보고 있는 단어가 찾고자 하는 단어인 경우
+        result += 1
+        index += len(word)
+    else:
+        index += 1
+print(result)
