16920a861b40acd8c394e7bcd6675fee6481587a
Untitled <mps86@naver.com>
2021-12-26 09:12:29 +0000

    백준 11724번 연결요소의 개수 DFS, BFS로 풀수있지만 유니온 파인드로도 풀 수 있음. 유니온파인드로 푼 문제

--- 11724.py
+++ 11724.py
@@ -21,15 +21,17 @@ def union(x,y):
 #n = 정점의 갯수, m = 간선의 갯수
 n,m = map(int,input().split())
 
-
+# 부모노드를 저장하는 배열.
 parent = [0]+[_ for _ in range(1,n+1)]
+# 연결 요소의 부모노드를 저장하는데 필요한 set
 ans_set = set()
 
+# 간선 m만큼 반복
 for _ in range(m):
     u,v = map(int,input().split())
     union(u,v)
     
 for i in range(1,len(parent)):
-    ans_set.add(parent[parent[i]])
+    ans_set.add(find(parent[i]))
 
 print(len(list(ans_set)))
