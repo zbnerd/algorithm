fd6d214e878bb13ea9829fb9b61b39e06810c5dc
Untitled <mps86@naver.com>
2022-01-13 12:02:52 +0000

    백준 2263번 트리의 순회 분할정복을 이용한 문제

--- 2263.py
+++ 2263.py
@@ -1,26 +1,34 @@
 import sys
 
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
-
 def chase_tree(istart,iend,pstart,pend):
-    if istart > iend or pstart > pend:
+    if istart > iend or pstart > pend :
         return
     
-    print(post_order_list[pend],' ')
-    inorder_offset = in_order_list.index(post_order_list[pend])
+    node = post_order[pend]
+    print(node,end=' ')
+    offset = in_order_pos[node]
     
+    left = offset-istart #왼쪽 트리 자식 갯수
+    right = iend-offset #오른쪽 트리 자식 갯수
     
-    #left child
-    chase_tree(istart,inorder_offset-1,pstart,inorder_offset-1)
-    #right child
-    chase_tree(inorder_offset+1,iend,inorder_offset,pend-1)
+    #left range = start ~ (start+left-1)
+    chase_tree(istart,istart+left-1,pstart,pstart+left-1)
     
-
+    #right range
+    #in_order end-right+1 ~ end
+    #post_order end-right ~ end-1
+    chase_tree(iend-right+1,iend,pend-right,pend-1)
+    
 n = int(input())
-in_order_list = list(map(int,input().split()))
-post_order_list = list(map(int,input().split()))
+in_order = list(map(int,input().split()))
+post_order = list(map(int,input().split()))
+in_order_pos = [0]*(n+1)
+
+for i in range(len(in_order)):
+    in_order_pos[in_order[i]] = i
 
 chase_tree(0,n-1,0,n-1)
