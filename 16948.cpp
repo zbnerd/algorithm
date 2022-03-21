2d7a4d1a9aaa2cfd14648af401ab04afc41e2a9f
Untitled <mps86@naver.com>
2022-03-21 11:07:11 +0000

    백준 16948번 데스나이트 BFS를 이용하면 풀 수 있는 문제

---
+++ 16948.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+int visited[201][201];
+int n;
+
+int bfs(pair<int, int> start, pair<int, int> end){
+	
+	visited[start.first][start.second] = 0;
+	queue<pair<int, int> > q;
+	
+	int dr[6] = {-2,-2,0,0,2,2};
+	int dc[6] = {-1,1,-2,2,-1,1};
+	
+	q.push(start);
+	
+	while(!q.empty()){
+		pair<int, int> poped = q.front(); q.pop();
+		int r = poped.first, c = poped.second;
+		
+		for(int k=0;k<6;k++){
+			int nr = r+dr[k], nc = c+dc[k];
+			
+			if((0 <= nr && nr < n) && (0 <= nc && nc < n)){
+				if(visited[nr][nc] == -1){
+					visited[nr][nc] = visited[r][c] + 1;
+					q.push(make_pair(nr, nc));
+				}
+			}
+		}
+	}
+	
+	return visited[end.first][end.second];
+}
+
+int main(){
+	
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	
+	for(int i=0;i<201;i++) memset(visited[i], -1, sizeof(int) * 201);
+	
+	int r1, c1, r2, c2;
+	cin >> n >> r1 >> c1 >> r2 >> c2;
+
+	cout << bfs(make_pair(r1, c1), make_pair(r2, c2));
+	return 0;
+}
