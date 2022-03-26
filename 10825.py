1384234edc4068b13ccf28cbd9de311130fa7dae
Untitled <mps86@naver.com>
2022-03-26 06:57:27 +0000

    백준 10825번 국영수 정렬 문제
    https://blog.naver.com/mps86/222683532250

--- 10825.py
+++ 10825.py
@@ -4,6 +4,7 @@ for _ in range(n):
     name, kor, eng, math = input().split()
     student.append([name, int(kor), int(eng), int(math)])
     
+    
 student.sort(key = lambda x : (-x[1],x[2], -x[3], x[0]))
 for c in student:
     print(c[0])
